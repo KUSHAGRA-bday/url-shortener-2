6292a260c55eab8f3a500480385fd64e2e489bb1
KUSHAGRA-bday <srivastavakushagra587@gmail.com>
2025-07-01 13:51:42 +0530

    Update page.jsx

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -5,10 +5,10 @@ const About = () => {
     <div>
       <main className='text-white flex flex-col items-center py-20 min-h-screen' >
       <h1 className='text-4xl font-bold underline'>About Us</h1>
-      <p className='text-lg px-70 py-5'>We are a team of passionate developers dedicated to creating the best URL shortener service. Our mission is to provide a simple, efficient, and user-friendly platform for all your URL shortening needs.</p>
+      <p className='text-lg px-70 py-5'>We are a team of passionate developers dedicated to creating the best URL shortener service. Our mission is to provide a simple, efficient, and user-friendly platform for all your URL shortening needs.Here u can see others URLs also as this is a cloud platform. This platform is for everyone who wants to share their links easily and efficiently. You can share your links with just a few clicks, and we'll take care of the rest and also if U want to promote your website so u can without any hesitation.</p>
       <br />
       <p className='text-lg px-70'>In a world full of noise and endless URLs, we bring clarity and simplicity. Our mission? Make every link memorable, trackable, and beautiful. From students to startups, anyone can use our short links to build smarter digital journeys. Built for developers, creators, and power users, this URL shortener is powered by [your tech stack, e.g. Node.js + MongoDB]. Every short link is encrypted, logged, and optionally tracked with analytics. APIs available. Privacy respected.</p></main>
-x    </div>
+    </div>
   )
 }
 
